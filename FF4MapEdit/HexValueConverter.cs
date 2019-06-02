485e69381049923b5e7c12db1393d21caf4c5beb
Entroper <entroper@gmail.com>
2019-06-02 17:21:57 -0400

    better formatting

---
+++ FF4MapEdit/HexValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FF4MapEdit
+{
+	public class HexValueConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is byte byteValue)
+				return byteValue.ToString("X2");
+
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is string stringValue)
+				return byte.Parse(stringValue, NumberStyles.HexNumber);
+
+			return null;
+		}
+	}
+}
